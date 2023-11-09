5bd658b091a454cdb76b89869ed98ad90bd47a10
VishnuVijay56 <75947385+VishnuVijay56@users.noreply.github.com>
2023-11-08 20:39:16 -0500

    Updated gym env and corresponding sim commands

--- sim_cmds.py
+++ sim_cmds.py
@@ -4,10 +4,17 @@ sim_cmds.py: command/message type for simulation
     - Created: 4/25/23
 """
 
+import numpy as np
+
 class SimCmds:
     def __init__(self):
         self.view_sim = False
         self.sim_real_time = False
         self.display_graphs = False
+        self.fullscreen = False
         self.use_kf = False
         self.wind_gust = False
+        self.no_wind = True
+        self.t_span = (0, 20)
+        self.Ts = 0.01
+        self.steady_state_wind = np.array([[0., 0., 0.]]).T
