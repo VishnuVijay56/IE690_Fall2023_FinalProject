30e32702cd0529c198b2733072c9c492da9dd37b
Bwodetzki <50225476+Bwodetzki@users.noreply.github.com>
2023-11-27 18:37:27 -0500

    Make small changes to train file

--- model_training/train_uav.py
+++ model_training/train_uav.py
@@ -1,24 +1,23 @@
-import gym
+import gymnasium as gym
 
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import SubprocVecEnv
-from stable_baselines import PPO2
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3 import PPO
 
 # multiprocess environment
 n_cpu = 4
 env = SubprocVecEnv([lambda: gym.make('CartPole-v1') for i in range(n_cpu)])
 
-model = PPO2(MlpPolicy, env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=25000)
 model.save("ppo2_cartpole")
 
 del model # remove to demonstrate saving and loading
 
-model = PPO2.load("ppo2_cartpole")
+model = PPO.load("ppo2_cartpole")
 
 # Enjoy trained agent
 obs = env.reset()
-while True:
+for i in range(1000):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render()
