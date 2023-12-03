76dc4cfe2c688ed1e0698f3f870aad1a47d6beff
Bwodetzki <50225476+Bwodetzki@users.noreply.github.com>
2023-12-02 19:19:23 -0500

    First draft of model evaluator

--- train_uav.py
+++ train_uav.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 
 from uav_gym_env import UAVStallEnv
 from sim_cmds import SimCmds
+from evaluate_env import model_evaluator
 
 # Simulation Parameters
 sim_opt = SimCmds()
@@ -23,9 +24,18 @@ model.learn(total_timesteps=10_000)
 
 # Evaluate Trained Agent
 vec_env = model.get_env()
-obs = vec_env.reset()
+obs, info = vec_env.reset()
+target = info
+
+# Initialize Evaluator
+evaluator = model_evaluator(obs, target)
+
 for i in range(1_000):
+    print(i)
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
+    evaluator.update(action, obs)
 
-print(vec_env.evaluate_model())
+something = evaluator.evaluate()
+
+print(env.evaluate_model())
